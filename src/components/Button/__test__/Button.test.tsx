067314e4e649f34851580d58727129033dbe3f40
Eric Johnsonn <ericjohnson@Erics-Mac-mini.lan>
2024-02-17 07:56:06 -0500

    fix unit test

--- src/components/Button/__test__/Button.test.tsx
+++ src/components/Button/__test__/Button.test.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
-import { Button } from "../Button";
+import Button from "../Button";
 
 describe("Button component", () => {
-  it("Button should render correctly", () => {
+  it("Button should render", () => {
     render(<Button />);
     const button = screen.getByRole("button");
     expect(button).toBeInTheDocument();
